d13e14fd2131f2b3a6e17500bebf9a52e52e4842
DigitaleWeltLibrary <DigitaleWeltLibrary@users.noreply.github.com>
2025-07-30 11:28:42 +0000

    deploy: f782c736677b7735762cf699075391bda828fa15

---
+++ assets/getword-DJoOq63r.js
@@ -0,0 +1,1 @@
+import{a}from"./PageHeader-CDBZ9QRZ.js";function n(r){r=r.toUpperCase();const t=a.alphabet.find(o=>o.letter===r);return t?t.word:"??"}export{n as g};
--- assets/getword-oLiZECAx.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./PageHeader-DOOrxwbG.js";function n(r){r=r.toUpperCase();const t=a.alphabet.find(o=>o.letter===r);return t?t.word:"??"}export{n as g};
